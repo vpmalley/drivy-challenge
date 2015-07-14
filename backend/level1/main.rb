6bb2f112ede8bbb25bc52bcacd151f62a904472c
Vincent Malley <vincent.malley@insalien.org>
2015-07-14 17:23:46 -0400

    finished level 1

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,32 @@
 require "json"
+require "date"
 
 # your code
+
+dataFile = File.new("data.json", "r");
+data = JSON.load(dataFile);
+dataFile.close();
+
+# making cars accessible as map (car_id > car)
+cars = [];
+for car in data["cars"] do
+  cars[car["id"]] = car;
+end
+
+# extracting rental information and storing it as a map
+output = {"rentals" => []};
+for rental in data["rentals"] do
+  # both first and last days are billed
+  nbDays = (Date.parse(rental["end_date"]) - Date.parse(rental["start_date"])).to_i + 1; 
+  pricePerDay = cars[rental["car_id"]]["price_per_day"];
+  pricePerKm = cars[rental["car_id"]]["price_per_km"];
+  nbKms = rental["distance"];
+  
+  price = nbDays * pricePerDay + nbKms * pricePerKm;
+  output["rentals"].push("id" => rental["id"], "price" => price);
+end
+
+# exporting rental info
+outFile = File.new("myoutput.json", "w");
+JSON.dump(output, outFile);
+outFile.close();
